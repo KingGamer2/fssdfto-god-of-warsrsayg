67c1b96f28a0d40b9ed36183fb9025c8ef5aff68
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-26 00:02:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "="
+const adminprefix = "--" 
 client.on("message", message => {
  
   function discoRole() {
